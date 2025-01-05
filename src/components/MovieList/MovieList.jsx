b5e5f665f60331b38b45b1c22d699441e9636728
Yurii <oniks9821@gmail.com>
2025-01-05 14:06:15 +0200

    1406

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import css from './MovieList.module.css';
 
-const MovieList = ({ movies, basePath = '/movies' }) => {
+const MovieList = ({ movies }) => {
   return (
     <ul className={css.movieList}>
       {movies.map(movie => (
         <li key={movie.id} className={css.movieItem}>
           <h3 className={css.subtitle}>
-            <Link to={`${basePath}/${movie.id}`} className={css.movieLink}>
+            <Link to={`/movies/${movie.id}`} className={css.movieLink}>
               {movie.title}
             </Link>
           </h3>
