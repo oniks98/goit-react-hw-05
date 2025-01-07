85a8f1b9095b755d640831a0576d549c7e1be6a7
Yurii <oniks9821@gmail.com>
2025-01-07 20:17:48 +0200

    2017

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <Link to="/">Go back to Home</Link>
+    <div className={css.blok}>
+      <h3 className={css.message}>404 - Page Not Found</h3>
+      <Link className={css.message} to="/">
+        Go back to Home
+      </Link>
     </div>
   );
 };
